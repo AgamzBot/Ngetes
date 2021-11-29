f0766865846670780adb815b4ec9b98fa166ec7f
AgamzBot <95152189+AgamzBot@users.noreply.github.com>
2021-11-30 05:35:03 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -3,14 +3,8 @@ const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
 1. *Alphabot Support*
-_https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
 
+https://chat.whatsapp.com/K9nXYo1R9so7BM4pR5Aywh
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
 `
